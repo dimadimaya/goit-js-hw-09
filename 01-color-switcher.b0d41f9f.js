266651a6df01996798f9f82872a7256cbc55ae6d
dimadimaya <103739636+dimadimaya@users.noreply.github.com>
2022-09-27 12:24:52 +0000

    Deploying to gh-pages from @ dimadimaya/goit-js-hw-09@838a1767485ff8ac083ceb7eee4b6bdb5b5a0912 🚀

---
+++ 01-color-switcher.b0d41f9f.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.body;function n(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}console.log(t),console.log(e),e.disabled=!0,t.addEventListener("click",(function(){timerId=setInterval(n,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(timerId),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.b0d41f9f.js.map
